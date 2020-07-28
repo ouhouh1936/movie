80989b5e76e518247ef5743c78ecb925f012584a
ouhouh1936 <ouhouh1936@gamil.com>
2020-07-28 22:47:28 +0900

    devops

---
+++ src/client/Root.js
@@ -0,0 +1,7 @@
+import React from "react";
+import App from "../components/App";
+import useTitle from "@4leaf.ysh/useTitle";
+
+return <App />;
+
+export default Root;
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDOM.render(<Root />, document.getElementById("App"));
